a3faad5c310db95d8633c586a8d1cfd6b3fbd142
Tony <tas@scoutgg.com>
2024-04-22 16:36:51 +0700

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 cp index.html ../zeddidragon/helldivers-2
 cp style.css ../zeddidragon/helldivers-2
+cp script.js ../zeddidragon/helldivers-2
+cp template.js ../zeddidragon/helldivers-2
+cp weapons.json ../zeddidragon/helldivers-2
