8aaf3d33588dbb450764b260a72d3f01715bcf4a
Zeddy <tas@scoutgg.com>
2024-04-24 20:45:06 +0700

    text cleanup

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ cp index.html ../zeddidragon/helldivers-2
 cp style.css ../zeddidragon/helldivers-2
 cp script.js ../zeddidragon/helldivers-2
 cp template.js ../zeddidragon/helldivers-2
-cp weapons.json ../zeddidragon/helldivers-2
+cp data/weapons.json ../zeddidragon/helldivers-2/data
