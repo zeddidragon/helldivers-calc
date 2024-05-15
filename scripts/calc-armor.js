c7b7eb384912f0762447a3b80841916a57011fbf
Tony <tas@scoutgg.com>
2024-05-15 15:54:30 +0700

    armor values

---
+++ scripts/calc-armor.js
@@ -0,0 +1,52 @@
+import fs from 'fs'
+
+const data = fs.readFileSync('data/damage-tests.txt', 'utf8')
+
+const hp = 125
+const limbs = {
+  Head: 2,
+  Chest: 0.85,
+  Arm: 0.7,
+  Leg: 0.8,
+}
+
+let armor = 0
+for(const line of data.split('\n')) {
+  const header = /(Light|Medium|Heavy) \((\d+)\)/.exec(line)
+  if(header) {
+    armor = +header[2]
+    continue
+  }
+  const domHit = /Dom (Head|Chest|Arm|Leg): (-?\d+)/.exec(line)
+  if(domHit) {
+    const base = 275
+    const [, limb, rem] = domHit
+    const damage = Math.abs(hp - rem)
+    const fraction = (damage / limbs[limb]) / base
+    console.log({ armor, limb, fraction })
+  }
+}
+
+const armors = {
+  50: 1.33,
+  64: 1.236,
+  70: 1.2,
+  79: 1.42,
+  100: 1.0,
+  150: 0.8,
+}
+
+const headers = ['Armor', ...Object.keys(limbs)]
+const rows = []
+for(const [armor, overall] of Object.entries(armors)) {
+  const row = [armor]
+  for(const [limb, multi] of Object.entries(limbs)) {
+    let base = overall
+    if(limb === 'Head') {
+      base = 1.0
+    }
+    row.push(`${(100 * base * multi).toFixed(2)}%`)
+  }
+  rows.push(row)
+}
+console.log([headers, ...rows].join('\n'))
