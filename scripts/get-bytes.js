ed9845b8f990d7d5d020ae4ef932968110e04239
Zeddy <tas@scoutgg.com>
2024-05-15 07:10:23 +0700

    dropoff tests

--- scripts/get-bytes.js
+++ scripts/get-bytes.js
@@ -1,10 +1,20 @@
 import fs from 'fs'
+import clipboard from 'clipboardy'
 
 const weapons = JSON.parse(fs.readFileSync('./data/weapons.json', 'utf-8').trim())
 
-const search = new RegExp(process.argv[2], 'i')
+let xMode = false
+let args = process.argv.slice(2)
+if(args[0] === 'x') {
+  xMode = true
+  args = args.slice(1)
+}
+const search = new RegExp(args[0], 'i')
 const wpn = weapons.find(wpn => search.exec(wpn.name))
-for(const override of process.argv.slice(3)) {
+if(!wpn) {
+  throw new Error(`No results: ${search}`)
+}
+for(const override of args.slice(1)) {
   const [prop, value] = override.split('=')
   wpn[prop] = +value
 }
@@ -33,10 +43,31 @@ const schema = Object.entries({
   effect2: Int,
   param2: Float,
 })
+const xschema = Object.entries({
+  xid: Int,
+  xdamage: Int,
+  xdurable: Int,
+  xap: Int,
+  xap2: Int,
+  xap3: Int,
+  xap4: Int,
+  xdemo: Int,
+  xstun: Int,
+  xpush: Int,
+  dmgtype: Int,
+  effect1: Int,
+  param1: Float,
+  effect2: Int,
+  param2: Float,
+})
+
+const useschema = xMode ? xschema : schema
 const buf = Buffer.alloc(0x4c)
 for(let i = 0; i < schema.length; i++) {
   const offset = i * 0x4
-  const [prop, cb] = schema[i]
+  const [prop, cb] = useschema[i]
   Int(buf, wpn[prop] || 0, offset)
 }
-console.log(buf)
+const str = Array.from(buf).map(byte => byte.toString(16).padStart(2, '0')).join(' ')
+console.log(str)
+clipboard.writeSync(str)
