fdbddcac4fc728ef6958ecba4f3281236de48c71
Tony <tas@scoutgg.com>
2024-05-17 11:30:34 +0700

    equipped with ballistics

---
+++ scripts/process-ballistics.js
@@ -0,0 +1,57 @@
+import fs from 'fs'
+
+const nameRex = /([\W|\w]+)\W+\(w\/ (\d+)x(\d+)mm ([\W|\w/]+)\)/ 
+const data = fs.readFileSync('data/ballistics.txt', 'utf8')
+
+const headers = [
+  'Weapon',
+  'Ammo',
+  'Caliber',
+  'Velocity',
+  'Mass',
+  'Drag',
+  'Gravity',
+  'Life',
+  'HitSlow',
+]
+
+const csv = [headers]
+
+for(const line of data.trim().split('\n').slice(1)) {
+  const [
+    weaponWithAmmo,
+    stats
+  ] = line.split('\t')
+  if(!stats) {
+    continue
+  }
+  const [
+    ,
+    name,
+    height,
+    length,
+    ammoName,
+  ] = nameRex.exec(weaponWithAmmo) || [, weaponWithAmmo]
+  const [
+    caliber,
+    velocity,
+    mass,
+    drag,
+    gravity,
+    life,
+    penSlow,
+  ] = stats.split(/\//)
+  csv.push([
+    name,
+    ammoName ? `${height}x${length}mm ${ammoName}` : '',
+    caliber,
+    velocity,
+    mass,
+    drag,
+    gravity,
+    life,
+    penSlow,
+  ].map(v => v?.trim() || ''))
+}
+
+fs.writeFileSync('data/ballistics.csv', csv.join('\n'))
