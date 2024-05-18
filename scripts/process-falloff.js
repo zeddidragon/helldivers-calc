36e3b957699d84f171ca385f11a36c2d8650576d
Zeddy <tas@scoutgg.com>
2024-05-18 10:17:58 +0700

    air velocity of a swaalow

--- scripts/process-falloff.js
+++ scripts/process-falloff.js
@@ -1,19 +1,17 @@
 import fs from 'fs'
-const data = fs.readFileSync('data/falloff-2.csv', 'utf8')
+const data = fs.readFileSync('data/falloff-3.csv', 'utf8')
 
 const [header, ...rows] = data
   .trim()
   .split('\n')
   .map(r => r.split(','))
 
-const timestep = 1 / 20
+const timestep = 1 / 60
 function simulation({ cal, v, m: mass, drag }) {
   let distance = 0
-  const airDensity = 1.293
-  const area = Math.PI * Math.pow(cal * 0.01, 2)
+  const area = Math.pow(cal * 0.01293, 2)
   for(let distance = 0; distance < 100;) {
     const dragForce = airDensity * Math.pow(v, 2) * drag * area
-    console.log(dragForce)
     const v2 = v - dragForce / mass * timestep
     if(v2 < 0) return 0
     distance += (+v + +v2) * 0.5 * timestep
@@ -24,7 +22,7 @@ function simulation({ cal, v, m: mass, drag }) {
 
 const hp = 20000
 const base = 10000
-const results = rows.map(([cal, v, m, drag, rem, comment]) => {
+const results = rows.map(([cal, v, m, drag, rem]) => {
   const dmg = hp - rem
   const ratio = (100 * (base - dmg) / base).toFixed(2)
   return [
@@ -34,8 +32,7 @@ const results = rows.map(([cal, v, m, drag, rem, comment]) => {
     drag,
     dmg,
     ratio,
-    simulation({ cal, v, m, drag }).toFixed(),
-    comment,
+    simulation({ cal, v, m, drag }).toFixed()
   ]
 })
 
@@ -50,15 +47,17 @@ console.log(results.map(r => {
     sim,
     comment,
   ] = r
-  return {
-    v: +v,
-    v2: v - +(v * ratio / 100).toFixed(),
-    sim: +sim,
-  }
-}))
+  return [
+    `Cal: ${+cal}`,
+    `Mass: ${+m}`,
+    `Vel: ${+v}`,
+    `Vel2: ${v - +(v * ratio / 100).toFixed()}`,
+    `Sim: ${sim}`,
+  ].map(v => v.padEnd(12)).join('')
+}).join('\n'))
 
 const output = [
   ['Caliber', 'Velocity', 'Mass', 'Drag', 'Damage', 'Falloff', 'Simulation', 'Commment'],
   ...results,
 ].join('\n')
-console.log(output)
+// console.log(output)
--- scripts/scribble.js
+++ scripts/scribble.js
@@ -1,10 +1,11 @@
 import clipboard from 'clipboardy'
 // 5.5 (calibre), 900 (speed), 4.5 (mass), 0.3 (drag) 
 
-let bytes = Buffer.alloc(4 * 3)
-bytes.writeFloatLE(900, 0)
-bytes.writeFloatLE(4.5, 4)
-bytes.writeFloatLE(0.3, 8)
+let bytes = Buffer.alloc(4 * 6)
+bytes.writeFloatLE(2000, 0x0) // Velocity
+bytes.writeFloatLE(50, 0x4) // Mass
+bytes.writeFloatLE(0.3, 0x8) // Drag
+bytes.writeFloatLE(1.0, 0xc) // Gravity
 
 const str = Array.from(bytes).map(byte => byte.toString(16).padStart(2, '0')).join(' ')
 console.log(str)
