12c8c317b1f7192f99355f8b889ca9a31eb6d6dd
Zeddy <tas@scoutgg.com>
2024-05-18 08:29:01 +0700

    simulatino a little closer

--- scripts/process-falloff.js
+++ scripts/process-falloff.js
@@ -5,26 +5,57 @@ const [header, ...rows] = data
   .trim()
   .split('\n')
   .map(r => r.split(','))
-console.log(header, rows)
+
+const timestep = 1 / 20
+function simulation({ cal, v, m: mass, drag }) {
+  let distance = 0
+  const airDensity = 1.293
+  const area = Math.PI * Math.pow(cal * 0.01, 2)
+  for(let distance = 0; distance < 100;) {
+    const dragForce = airDensity * Math.pow(v, 2) * drag * area
+    console.log(dragForce)
+    const v2 = v - dragForce / mass * timestep
+    if(v2 < 0) return 0
+    distance += (+v + +v2) * 0.5 * timestep
+    v = v2
+  }
+  return v
+}
 
 const hp = 20000
 const base = 10000
 const results = rows.map(([cal, v, m, drag, rem, comment]) => {
   const dmg = hp - rem
   const ratio = (100 * (base - dmg) / base).toFixed(2)
-  const simulation = (2 * Math.PI * cal * cal * drag / m)
   return [
     cal,
     v,
     m,
     drag,
     dmg,
-    `${ratio}%`,
-    simulation.toFixed(2),
+    ratio,
+    simulation({ cal, v, m, drag }).toFixed(),
     comment,
   ]
 })
 
+console.log(results.map(r => {
+  const [
+    cal,
+    v,
+    m,
+    drag,
+    dmg,
+    ratio,
+    sim,
+    comment,
+  ] = r
+  return {
+    v: +v,
+    v2: v - +(v * ratio / 100).toFixed(),
+    sim: +sim,
+  }
+}))
 
 const output = [
   ['Caliber', 'Velocity', 'Mass', 'Drag', 'Damage', 'Falloff', 'Simulation', 'Commment'],
