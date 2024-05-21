3405f72fd4c85cfac11a46941364cef481e1dd13
Zeddy <tas@scoutgg.com>
2024-05-21 09:42:26 +0700

    200 armor

--- scripts/scribble.js
+++ scripts/scribble.js
@@ -2,10 +2,10 @@ import clipboard from 'clipboardy'
 // 5.5 (calibre), 900 (speed), 4.5 (mass), 0.3 (drag) 
 
 let bytes = Buffer.alloc(4 * 4)
-bytes.writeFloatLE(900, 0x0) // Velocity
-bytes.writeFloatLE(4.5, 0x4) // Mass
-bytes.writeFloatLE(0.3, 0x8) // Drag
-bytes.writeFloatLE(1.0, 0xc) // Gravity
+bytes.writeFloatLE(90, 0x0) // Velocity
+bytes.writeFloatLE(100, 0x4) // Mass
+bytes.writeFloatLE(4, 0x8) // Drag
+bytes.writeFloatLE(3.0, 0xc) // Gravity
 
 const str = Array.from(bytes).map(byte => byte.toString(16).padStart(2, '0')).join(' ')
 console.log(str)
