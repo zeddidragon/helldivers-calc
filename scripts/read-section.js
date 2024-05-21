a9e06635c9f21d1e5f74c0b93433af4cc591cc26
Tony <tas@scoutgg.com>
2024-05-22 00:32:24 +0700

    mine ballistics data

--- scripts/read-section.js
+++ scripts/read-section.js
@@ -13,6 +13,20 @@ const Float = {
   write: (buf, v, off = 0) => buf.writeFloatLE(v, off),
   size: 0x04,
 }
+const Unknown = {
+  read: (buf, off = 0) => {
+    const float = buf.readFloatLE(off)
+    const int = buf.readInt32LE(off)
+    const hex = buf.slice(off, off + 0x8)
+      .map(v => v.toString(16).padStart(2, '0'))
+      .join('')
+    return void 0
+  },
+  write: (buf, v, off = 0) => {
+    throw Error('Write not implemented')
+  },
+  size: 0x04,
+}
 
 const strikeSchema = {
   id: Int,
@@ -36,7 +50,7 @@ const strikeSchema = {
   param4: Float,
 }
 const strikeValues = Object.values(strikeSchema)
-const BLOCK_SIZE = strikeValues.reduce((sum, v) => sum + v.size, 0)
+const STRIKE_SIZE = strikeValues.reduce((sum, v) => sum + v.size, 0)
 
 const dominatorStrike = [
   84,
@@ -50,7 +64,9 @@ const dominatorStrike = [
   0, 0,
 ]
 
-const ballisticSchema = {
+const ballisticSearchSchema = {
+  caliber: Float,
+  i1: Int,
   velocity: Float,
   mass: Float,
   drag: Float,
@@ -58,10 +74,14 @@ const ballisticSchema = {
 }
 
 const liberatorBallistics = [
+  5.5,
+  1,
   900,
   4.5,
   0.3,
   1.0,
+  0,
+  0.25,
 ]
 
 function getRow(data, schema) {
@@ -83,19 +103,25 @@ function hex(v) {
   return `0x${v.toString(16).toUpperCase()}`
 }
 
+function rewind(buffer, idx, blockSize) {
+  while(buffer.readInt32LE(idx - blockSize)) {
+    idx -= blockSize
+  }
+  return idx
+}
+
 const domBytes = getRow(dominatorStrike, strikeSchema)
 let idx = buffer.indexOf(domBytes.slice(strikeValues[0].size))
 if(idx < 0) {
   throw new Error('Unable to locate Dominator Strike!')
 }
-idx -= 0x4
+idx -= 0x4 // Hop back to ID
 console.log(`Dominator located at: ${hex(idx)}`)
-const dominatorId = buffer.readInt32LE(idx)
+const dominatorId = Int.read(buffer, idx)
 console.log(`Dominator ID: ${hex(dominatorId)}`)
-idx -= (dominatorId - 1) * BLOCK_SIZE
+idx = rewind(buffer, idx, STRIKE_SIZE) // Walk back to first row
 console.log(`Strike Data starts at: ${hex(idx)}`)
 
-let prevId = 0
 const strikes = []
 while(true) {
   const strike = {}
@@ -105,21 +131,62 @@ while(true) {
   }
   if(strike.id) {
     strikes.push(strike)
-    prevId = strike.id
   } else {
     break
   }
 }
 
-const libBytes = getRow(liberatorBallistics, ballisticSchema)
+const BALLISTIC_SIZE = 0xf0
+const libBytes = getRow(liberatorBallistics, ballisticSearchSchema)
+console.log(libBytes)
 idx = buffer.indexOf(libBytes)
 if(idx < 0) {
-  throw new Error('Unable to locate 5.5mm ballstics!')
+  throw new Error('Unable to locate 5.5mm ballistics!')
 }
 console.log(`5.5mm located at: ${hex(idx)}`)
+idx += 0x24 - BALLISTIC_SIZE // Hop back to ID
+const libId = Int.read(buffer, idx)
+console.log(`5.5mm ID: ${hex(libId)}`)
+idx = rewind(buffer, idx, BALLISTIC_SIZE)
+console.log(`Ballistics Data starts at: ${hex(idx)}`)
+
+function unknowns(from, to) {
+  const obj = {}
+  for(let i = from; i <= to; i++) {
+    obj[`unk${i}`] = Unknown
+  }
+  return obj
+}
+
+const ballisticSchema = {
+  id: Int,
+  ...unknowns(2, 51),
+  caliber: Float,
+  unk53: Int,
+  velocity: Float,
+  mass: Float,
+  drag: Float,
+  gravity: Float,
+  ...unknowns(58, 60)
+}
+
+const ballistics = []
+while(true) {
+  const obj = {}
+  for(const [prop, type] of Object.entries(ballisticSchema)) {
+    obj[prop] = type.read(buffer, idx)
+    idx += type.size
+  }
+  if(obj.id) {
+    ballistics.push(obj)
+  } else {
+    break
+  }
+}
 
 const data = JSON.stringify({
   version,
   strikes,
+  ballistics,
 }, null, 2)
 fs.writeFileSync('./data/datamined.json', data)
