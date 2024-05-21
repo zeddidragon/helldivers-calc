375924ca14f916b26274c1e732d93a5dc6138c98
Zeddy <tas@scoutgg.com>
2024-05-21 22:36:49 +0700

    get same data as before

--- scripts/read-section.js
+++ scripts/read-section.js
@@ -3,12 +3,17 @@ import fs from 'fs'
 const version = process.argv[3]
 const buffer = fs.readFileSync(process.argv[2])
 
-function Int(buffer, offset = 0) {
-  return buffer.readInt32LE(offset)
+const Int = {
+  read: (buf, off = 0) => buf.readInt32LE(off),
+  write: (buf, v, off = 0) => buf.writeInt32LE(v, off),
+  size: 0x04,
 }
-function Float(buffer, offset = 0) {
-  return buffer.readFloatLE(offset)
+const Float = {
+  read: (buf, off = 0) => buf.readFloatLE(off),
+  write: (buf, v, off = 0) => buf.writeFloatLE(v, off),
+  size: 0x04,
 }
+
 const schema = {
   id: Int,
   dmg: Int,
@@ -25,26 +30,66 @@ const schema = {
   param1: Float,
   func2: Int,
   param2: Float,
+  func3: Int,
+  param3: Float,
+  func4: Int,
+  param4: Float,
 }
-const valueSize = 0x4
-function readWeapon(buffer) {
-  const obj = {}
+const schemaValues = Object.values(schema)
+const BLOCK_SIZE = schemaValues.reduce((sum, v) => sum + v.size, 0)
+
+const dominatorStrike = [
+  84,
+  275, 90,
+  3, 3, 3, 0,
+  10, 35, 15,
+  0,
+  0, 0,
+  0, 0,
+  0, 0,
+  0, 0,
+]
+
+function strikeRow(data) {
+  const buf = Buffer.alloc(BLOCK_SIZE)
+  let i = 0
   let offset = 0
-  for(const [k, cb] of Object.entries(schema)) {
-    obj[k] = cb(buffer, offset)
-    offset += valueSize
+  for(const { write: cb, size } of schemaValues) {
+    const value = data[i] || 0
+    cb(buf, value, offset)
+    i++
+    offset += size
   }
-  return obj
+  return buf
 }
 
-const start = 0x64
-const pageSize = 0x4c
+function hex(v) {
+  return `0x${v.toString(16).toUpperCase()}`
+}
+
+const domBytes = strikeRow(dominatorStrike)
+let idx = buffer.indexOf(domBytes.slice(schemaValues[0].size))
+if(idx < 0) {
+  throw new Error('Unable to locate Dominator!')
+}
+idx -= 0x4
+console.log(`Dominator located at: ${hex(idx)}`)
+const dominatorId = buffer.readInt32LE(idx)
+console.log(`Dominator ID: ${hex(dominatorId)}`)
+idx -= (dominatorId - 1) * BLOCK_SIZE
+console.log(`Strike Data starts at: ${hex(idx)}`)
+
+let prevId = 0
 const strikes = []
-for(let i = start; i < buffer.length; i += pageSize) {
-  const page = buffer.slice(i, i + pageSize)
-  const strike = readWeapon(page)
+while(true) {
+  const strike = {}
+  for(const [prop, type] of Object.entries(schema)) {
+    strike[prop] = type.read(buffer, idx)
+    idx += type.size
+  }
   if(strike.id) {
     strikes.push(strike)
+    prevId = strike.id
   } else {
     break
   }
