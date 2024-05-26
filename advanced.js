bc999c93c11e8f9182daca4170c81deac6baec11
Tony <tas@scoutgg.com>
2024-05-26 18:48:06 +0700

    sorting

--- advanced.js
+++ advanced.js
@@ -1,9 +1,77 @@
+function val(obj, prop) {
+  return obj[prop] || 0
+}
+
+function sorting(col) {
+  let dir = -1
+  if(['id', 'damageid', 'name'].includes(col)) {
+    dir = 1
+  }
+
+  function idSort(a, b) {
+    return a.id - b.id
+  }
+
+  if(col === 'name') {
+    return function sortByName(a, b) {
+      const diff = (a.name || '').localeCompare(b.name || '') * dir
+      return diff || idSort(a, b)
+    }
+  }
+
+  function compare(a, b, prop) {
+    return (val(a, prop) - val(b, prop)) * dir
+  }
+
+  if(col === 'dmg') {
+    return function dmgSort(a, b) {
+      return compare(a, b, 'dmg') || compare(a, b, 'mass') || idSort(a, b)
+    }
+  }
+  if(col === 'ap') {
+    return function apSort(a, b) {
+      for(let i = 1; i <= 4; i++) {
+        const diff = compare(a, b, `ap${i}`)
+        if(diff) return diff
+      }
+      return idSort(a, b)
+    }
+  }
+  if(col === 'radius') {
+    return function radiusSort(a, b) {
+      for(let i = 1; i <= 3; i++) {
+        const diff = compare(a, b, `r${i}`)
+        if(diff) return diff
+      }
+      return idSort(a, b)
+    }
+  }
+  if(col === 'effect') {
+    return function apSort(a, b) {
+      for(let i = 1; i <= 4; i++) {
+        const eDiff = compare(a, b, `func${i}`)
+        if(eDiff) return eDiff
+        const pDiff = compare(a, b, `param${i}`)
+        if(pDiff) return pDiff
+      }
+      return idSort(a, b)
+    }
+  }
+  return function defaultSort(a, b) {
+    return compare(a, b, col) || idSort(a, b)
+  }
+}
+
 window.locals = {
+  sorting: 'id',
   id: (obj, prop='id') => {
     const v = obj[prop]
     if(!v) return
 
     return v.toString(16).toUpperCase()
+  },
+  sorted: (arr) => {
+    return arr.sort(sorting(locals.sorting))
   },
   allExplosion: () => {
     return data.explosions
@@ -92,7 +160,18 @@ function enumName(scope, obj) {
 }
 
 async function loadData() {
-  window.data = (await fetch(`data/datamined.json`).then(res => res.json()))
+  const [
+    manual,
+    mined,
+  ] = await Promise.all([
+    fetch(`data/manual.json`).then(res => res.json()),
+    fetch(`data/datamined.json`).then(res => res.json()),
+  ])
+  const data = {
+    ...manual,
+    ...mined,
+  }
+  window.data = data
   locals.damages = data.damages.map(obj => {
     return {
       ...obj,
@@ -123,12 +202,32 @@ async function loadData() {
   })
 }
 
+function sortBy(col) {
+  return function sort() {
+    locals.sorting = col
+    render()
+  }
+}
+
 function render() {
   document.querySelector('body').innerHTML = template(locals)
+  const headers = document.querySelectorAll('th')
+  for(const h of headers) {
+    const prop = h.classList[1]
+    if(prop === locals.sorting) {
+      h.classList.add('sorting')
+    }
+    h.addEventListener('click', sortBy(prop))
+    const chevron = document.createElement('span')
+    chevron.classList.add('sorter')
+    chevron.textContent = '▼'
+    h.appendChild(chevron)
+  }
 }
 
 window.switchScope = function switchScope(scope) {
   locals.nerdScope = scope
+  locals.sorting = 'id'
   render()
 }
 
--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ cp script.js ../zeddidragon/helldivers-2
 cp build/template.js ../zeddidragon/helldivers-2
 cp data/weapons.json ../zeddidragon/helldivers-2/data
 cp data/datamined.json ../zeddidragon/helldivers-2/data
-cp build/main.js ../zeddidragon/helldivers-2/
+cp build/main.js ../zeddidragon/helldivers-2/build
 cp advanced.html ../zeddidragon/helldivers-2/
-cp src/main.js ../zeddidragon/helldivers-2/advanced.js
+cp advanced.js ../zeddidragon/helldivers-2/
